d0eb041a0dd121f3b7ac899b23c60c10041ff418
Ryan Lopopolo <rjl@hyperbo.la>
2021-08-03 18:26:24 -0700

    Sync Rakefile clippy with CI clippy, fix lint violation

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,4 @@
 #![doc(html_root_url = "https://docs.rs/ruby-file-expand-path/0.1.0")]
 
 #[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+mod tests {}
