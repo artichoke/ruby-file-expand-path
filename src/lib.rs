81f7cf6c2cf1881bec880e3af4f932df16809f77
Ryan Lopopolo <rjl@hyperbo.la>
2022-02-10 08:35:02 -0800

    Remove target limits from package.metadata.docs.rs, add doc_cfg
    `ruby-file-expand-path` _does_ have different code on different targets,
    so don't limit the docs.rs targets to Linux.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,12 @@
+// Enable feature callouts in generated documentation:
+// https://doc.rust-lang.org/beta/unstable-book/language-features/doc-cfg.html
+//
+// This approach is borrowed from tokio.
+#![cfg_attr(docsrs, feature(doc_cfg))]
+#![cfg_attr(docsrs, feature(doc_alias))]
+
+//! WIP
+
 #![doc(html_root_url = "https://docs.rs/ruby-file-expand-path/0.1.0")]
 
 #[cfg(test)]
